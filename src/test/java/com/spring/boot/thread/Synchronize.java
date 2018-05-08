eb6cdd3da68c87509aa36ec52063f3822774ee76
gao1399677 <gao.shengsheng@qq.com>
2018-05-08 11:16:21 +0800

    synchronize

---
+++ src/test/java/com/spring/boot/thread/Synchronize.java
@@ -0,0 +1,18 @@
+package com.spring.boot.thread;
+
+/**
+ * <p>Title: Synchronize </p>
+ * <p>Description: Synchronize </p>
+ * Date: 2018年05月08日 下午11:08 AM
+ *
+ * @author gaosheng@hiynn.com
+ * @version 1.0 </p>
+ * Significant Modify：
+ * Date               Author           Content
+ * ==========================================================
+ * 2018年05月08日       SGao        创建文件,实现基本功能
+ * <p>
+ * ==========================================================
+ */
+public class Synchronize {
+}
